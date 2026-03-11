13875942254a575c0b2edab16a5f7dbc74b9a570
umnakhawaja <khawajau@oregonstate.edu>
2026-03-11 14:10:02 -0700

    created outline for schedules.py file - based on section 3 of project notes

---
+++ Our code/schedules.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import math
+from typing import Callable, List, Optional, Tuple
+
+import torch
+import torch.nn.functional as F
+
+# start with computation helpers 
+
+
+# default smoothness functions
+
+# GA implementation (Algorithm 1)
+
+# GC implementation (algorithm 2)
+
+# schedule builder / utilities.
